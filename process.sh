25a94f03409600166a8952596ceb3a6cb22e88a3
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-06-10 12:06:10 +0100

    Update scripts to parse command line args

--- process.sh
+++ process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "***********" $1 "************"
-java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1 s &&
-java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1 p 100 &&
-java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1 p 200 &&
-java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1 p 500 &&
-java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar $1 p 1000
+java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar -f $1 -s &&
+java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar -f $1 -p 100 &&
+java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar -f $1 -p 200 &&
+java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar -f $1 -p 500 &&
+java -jar target/uk.ac.imperial-1.0.0-SNAPSHOT-jar-with-dependencies.jar -f $1 -p 1000
--- src/main/java/script/Main.java
+++ src/main/java/script/Main.java
@@ -1,43 +1,94 @@
 package script;
 
+import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import org.apache.commons.cli.*;
 import pipe.reachability.algorithm.*;
 import pipe.reachability.algorithm.parallel.MassiveParallelStateSpaceExplorer;
 import pipe.reachability.algorithm.sequential.SequentialStateSpaceExplorer;
-import uk.ac.imperial.io.KryoStateIO;
-import uk.ac.imperial.io.StateIOProcessor;
-import uk.ac.imperial.io.StateProcessor;
+import pipe.steadystate.algorithm.SteadyStateBuilder;
+import pipe.steadystate.algorithm.SteadyStateBuilderImpl;
+import pipe.steadystate.algorithm.SteadyStateSolver;
+import uk.ac.imperial.io.*;
 import uk.ac.imperial.pipe.exceptions.InvalidRateException;
 import uk.ac.imperial.pipe.io.PetriNetIOImpl;
 import uk.ac.imperial.pipe.io.PetriNetReader;
 import uk.ac.imperial.pipe.models.petrinet.PetriNet;
 import uk.ac.imperial.pipe.parsers.UnparsableException;
+import uk.ac.imperial.state.ClassifiedState;
+import uk.ac.imperial.state.Record;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Class used to run state space exploration and optionally steady state solver
+ */
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static final Options options = new Options();
+    static {
+        options.addOption("f", true, "File to parse");
+        options.addOption("s", false, "Run sequentially");
+        options.addOption("p", true, "Run in parallel with the specified number of threads per state");
+        options.addOption("ss", false, "Solve the steady state with jacobi");
+        options.addOption("b", true, "Bound for Jacobi");
+    }
+
+    /**
+     *
+     * Args should be:
+     *  - filename e.g. models/foo.xml
+     *  - algorithm p => parallel, s => sequential
+     *  - solve steady state too? y => yes n => no
+     *  - if processing in parallel number of threads allowed
+     *  -
+     *
+     *
+     * @param args
+     * @throws JAXBException
+     * @throws UnparsableException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws InvalidRateException
+     * @throws TimelessTrapException
+     * @throws IOException
+     */
     public static void main(String[] args)
             throws JAXBException, UnparsableException, InterruptedException, ExecutionException, InvalidRateException,
-            TimelessTrapException, IOException {
-        String file = args[0];
-        String algo = args[1];
-
-        PetriNet petriNet = readPetriNet(file);
-        if (algo.equals("s")) {
-            processSequential(petriNet);
+            TimelessTrapException, IOException, ParseException {
+        CommandLineParser parser = new GnuParser();
+        CommandLine cmd = parser.parse( options, args);
+        if (cmd.hasOption("f")) {
+            String filename = cmd.getOptionValue("f");
+            PetriNet petriNet = readPetriNet(filename);
+            if (cmd.hasOption("s")) {
+                processSequential(petriNet, cmd);
+            } else {
+                int states;
+                if (cmd.hasOption("p")) {
+                    states = Integer.parseInt(cmd.getOptionValue("p"));
+                } else {    //Default
+                    states = 100;
+                }
+                processParallel(petriNet, states, cmd);
+            }
         } else {
-            int states = Integer.parseInt(args[2]);
-            processParallel(petriNet, states);
+           LOGGER.log(Level.SEVERE, "Need a file name");
         }
-
-
-
-
     }
 
     public static PetriNet readPetriNet(String path) throws JAXBException, UnparsableException, IOException {
@@ -45,7 +96,7 @@ public class Main {
         return io.read(path);
     }
 
-    private static void processParallel(PetriNet petriNet, int statesPerThread)
+    private static void processParallel(PetriNet petriNet, int statesPerThread, CommandLine cmd)
             throws IOException, InterruptedException, TimelessTrapException, ExecutionException, InvalidRateException {
 
         KryoStateIO kryoIo = new KryoStateIO();
@@ -64,27 +115,52 @@ public class Main {
                 explore(stateSpaceExplorer, explorerUtilities, " Parallel " + statesPerThread);
 
             }
-            //            try (InputStream transitionInputStream = Files.newInputStream(transitions);
-            //                 InputStream stateStream = Files.newInputStream(state);
-            //                 Input inputStream = new Input(transitionInputStream);
-            //                 Input stateInputStream = new Input(stateStream)) {
-            //                MultiStateReader reader = new EntireStateReader(kryoIo);
-            //                List<Record> records = new ArrayList<>(reader.readRecords(inputStream));
-            //                Map<Integer, ClassifiedState> mappings = reader.readStates(stateInputStream);
-            //
-            //                SteadyStateBuilder builder = new SteadyStateBuilderImpl();
-            //                ParallelSteadyStateSolver solver = new ParallelSteadyStateSolver(8, builder);
-            //                Map<Integer, Double> steadyState = solve(solver, records, "Parallel");
-            //                System.out.println("----------------------");
-            ////                GaussSeidelSolver gaussSeidelSolver = new GaussSeidelSolver();
-            ////                solve(gaussSeidelSolver, records, "Gauss Seidel");
-            //
-            //
-            //            }
+            if (cmd.hasOption("ss")) {
+                solveSteadyState(kryoIo, transitions, state, cmd);
+            }
         }
 
     }
-    private static void processSequential(PetriNet petriNet)
+
+    private static void solveSteadyState(KryoStateIO kryoIo, Path transitions, Path state, CommandLine cmd) throws IOException {
+        try (InputStream transitionInputStream = Files.newInputStream(transitions);
+             InputStream stateStream = Files.newInputStream(state);
+             Input inputStream = new Input(transitionInputStream);
+             Input stateInputStream = new Input(stateStream)) {
+            MultiStateReader reader = new EntireStateReader(kryoIo);
+            List<Record> records = new ArrayList<>(reader.readRecords(inputStream));
+            Map<Integer, ClassifiedState> mappings = reader.readStates(stateInputStream);
+
+            SteadyStateBuilder builder = new SteadyStateBuilderImpl();
+            ExecutorService executorService = null;
+            SteadyStateSolver solver;
+            if (cmd.hasOption("s")) {
+                if (cmd.hasOption("b")) {
+                    Integer maxIterations = Integer.valueOf(cmd.getOptionValue("b"));
+                    solver = builder.buildBoundedSequentialJacobi(maxIterations);
+                } else {
+                    solver = builder.buildSequentialJacobi();
+                }
+            } else {
+                executorService = Executors.newFixedThreadPool(8);
+                if (cmd.hasOption("b")) {
+                    Integer maxIterations = Integer.valueOf(cmd.getOptionValue("b"));
+                    solver = builder.buildBoundedParallelJacobiSolver(executorService, 8, maxIterations);
+                } else {
+                    solver = builder.buildJacobi(executorService, 8);
+                }
+            }
+            Map<Integer, Double> steadyState = solve(solver, records, "Parallel");
+            if (executorService != null) {
+                executorService.shutdownNow();
+            }
+        }
+    }
+    private static Map<Integer, Double> solve(SteadyStateSolver solver, List<Record> records, String method) {
+        return solver.solve(records);
+    }
+
+    private static void processSequential(PetriNet petriNet, CommandLine cmd)
             throws IOException, InterruptedException, TimelessTrapException, ExecutionException, InvalidRateException {
 
         KryoStateIO kryoIo = new KryoStateIO();
@@ -102,20 +178,9 @@ public class Main {
 
                 explore(stateSpaceExplorer, explorerUtilities, " Sequential ");
             }
-            //            try (InputStream transitionInputStream = Files.newInputStream(transitions);
-            //                 InputStream stateStream = Files.newInputStream(state);
-            //                 Input inputStream = new Input(transitionInputStream);
-            //                 Input stateInputStream = new Input(stateStream)) {
-            //                MultiStateReader reader = new EntireStateReader(kryoIo);
-            //                List<Record> records = new ArrayList<>(reader.readRecords(inputStream));
-            //                Map<Integer, ClassifiedState> mappings = reader.readStates(stateInputStream);
-            //
-            //
-            //                GaussSeidelSolver solver = new GaussSeidelSolver();
-            ////                Map<Integer, Double> steadyState = solve(solver, records, "Gauss Seidel");
-            //
-            //
-            //            }
+            if (cmd.hasOption("ss")) {
+                solveSteadyState(kryoIo, transitions, state, cmd);
+            }
         }
     }
 
