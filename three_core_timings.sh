4ec8f769791fedc2fd7ac595b6016e7956abb14b
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2014-06-13 12:42:28 +0100

    Echo cores

--- three_core_timings.sh
+++ three_core_timings.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # $1 = directory
+echo "***** 2 cores *****"
 ./timing_tests.sh $1 2 &&
-./timing_tests.sh $1 4 &&
+echo "***** 4 cores *****" &&
+./timing_tests.sh $1 4 && 
+echo "***** 8 cores *****" &&
 ./timing_tests.sh $1 8 
